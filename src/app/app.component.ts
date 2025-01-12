774afa946c952c3b9dc6f493b24fa1409a02917d
Alex Crist <alex@alex-crist.com>
2025-01-11 22:53:08 -0600

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { PrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,18 @@ import { ButtonModule } from 'primeng/button';
 })
 export class AppComponent {
   title = 'price-alert';
+
+  constructor(private primeng: PrimeNG) {
+    this.primeng.theme.set({
+      preset: Aura,
+      options: {
+        cssLayer: {
+          name: 'primeng',
+          order: 'tailwind-base, primeng, tailwind-utilities',
+        },
+      },
+    });
+  }
 
   openWebsite() {
     window.open('https://alex-crist.com', '_blank');
