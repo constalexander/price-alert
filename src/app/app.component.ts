3f17dec429e991fe6e3b779e87557a7103b82168
Alex Crist <alex@alex-crist.com>
2025-01-11 22:42:30 -0600

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'price-alert';
+
+  openWebsite() {
+    window.open('https://alex-crist.com', '_blank');
+  }
 }
