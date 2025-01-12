dd66ab82152e9fa826f0192a656aea4087c8a838
Alex Crist <alex@alex-crist.com>
2025-01-11 19:23:13 -0600

    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'price-alert-ng' title`, () => {
+  it(`should have the 'price-alert' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('price-alert-ng');
+    expect(app.title).toEqual('price-alert');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, price-alert-ng');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, price-alert'
+    );
   });
 });
