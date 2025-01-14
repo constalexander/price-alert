59cbea661eb030e83ee3b17dd5a61d477d2cedde
Alex Crist <alex@alex-crist.com>
2025-01-13 20:20:05 -0600

    .

--- src/app/core/services/metal.service.ts
+++ src/app/core/services/metal.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
-import { environment } from '@env/environment';
+import { environment } from '@/env/environment';
 
 export interface MetalPriceResponse {
   success: boolean;
