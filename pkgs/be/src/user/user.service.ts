8a66ada111a7e68f68ef0898e09eee927e20eb84
Alex Crist <alex@alex-crist.com>
2025-01-18 16:13:35 -0600

    .

--- pkgs/be/src/user/user.service.ts
+++ pkgs/be/src/user/user.service.ts
@@ -3,6 +3,17 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UserService {
   healthCheck(): string {
-    return `Price Check User API is running as of ${new Date().toISOString()}`;
+    const date = new Date();
+    const formattedDate = date.toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    });
+    const formattedTime = date.toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true,
+    });
+    return `Price Check User API is running as of ${formattedDate} ${formattedTime}`;
   }
 }
