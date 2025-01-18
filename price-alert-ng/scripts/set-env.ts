0def0b7c62a830c820e8e8ca3fd42f73218786e9
Alex Crist <alex@alex-crist.com>
2025-01-18 11:11:07 -0600

    pnpm workspace 1

--- price-alert-ng/scripts/set-env.ts
+++ price-alert-ng/scripts/set-env.ts
@@ -2,17 +2,14 @@ const dotenv = require('dotenv');
 const fs = require('fs');
 const path = require('path');
 
-// Load environment variables from .env-local
-dotenv.config({ path: '.env-local' });
+dotenv.config({ path: '../.env-local' });
 
-// Create the environment file content
 const environmentFileContent = `export const environment = {
   production: false,
   METAL_PRICE_API_KEY: '${process.env['METAL_PRICE_API_KEY'] || ''}',
 };
 `;
-
-// Write the environment file
-fs.writeFileSync(path.join(__dirname, '../src/environments/environment.ts'), environmentFileContent);
+const environmentFilePath = path.join(__dirname, '../src/environments/environment.ts');
+fs.writeFileSync(environmentFilePath, environmentFileContent);
 
 console.log('Environment file generated!');
---
+++ price-alert-ng/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  METAL_PRICE_API_KEY: '9cacd7536bf76fd3d948ce2a99bfcf29',
+};
