80069d857c8b66f1b5fed9534d5becd9877e99c7
Alex Crist <alex@alex-crist.com>
2025-01-18 21:08:12 -0600

    .

--- pkgs/be/src/prisma.service.ts
+++ pkgs/be/src/prisma.service.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 import * as dotenv from 'dotenv';
 
 // Load environment variables from root .env-local
-const envPath = path.resolve(__dirname, '../../../.env-local');
+const envPath = path.resolve(__dirname, '/.env');
 dotenv.config({ path: envPath });
 
 @Injectable()
