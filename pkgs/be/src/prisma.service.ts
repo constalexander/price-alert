6fe9a8999af1a49c48f16f3ceb20966e3bd74982
Alex Crist <alex@alex-crist.com>
2025-01-18 18:09:30 -0600

    .

--- pkgs/be/src/prisma.service.ts
+++ pkgs/be/src/prisma.service.ts
@@ -1,8 +1,23 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from root .env-local
+dotenv.config({ path: path.resolve(__dirname, '../../../.env-local') });
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.MONGODB_ATLAS_CONNECTION_STRING,
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
