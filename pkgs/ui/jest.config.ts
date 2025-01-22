366052d5251b086fa7436bf2d81e0b1e7fd88a48
Alex Crist <alex@alex-crist.com>
2025-01-21 23:42:04 -0600

    .

--- pkgs/ui/jest.config.ts
+++ pkgs/ui/jest.config.ts
@@ -5,6 +5,10 @@ const jestConfig: Config = {
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
   globalSetup: 'jest-preset-angular/global-setup',
+  moduleNameMapper: {
+    '^@/env/(.*)$': '<rootDir>/src/environments/$1',
+    '^@/(.*)$': '<rootDir>/src/app/$1',
+  },
 };
 
 export default jestConfig;
